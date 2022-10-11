cc899a4e7527b3be7f979de0f48c33829af8d082
Jasowills <Jujuzhaddy01@gmail.com>
2022-10-11 15:01:04 +0100

    origin

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('keydown', playSound);
+
+// To add!!!
+// window.addEventListener('click', playSound); play sound on click
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('make');
+}
+
+function removeTransition(e) {
+    if (e.propertyName === 'box-shadow') return;
+    this.classList.remove('make');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
